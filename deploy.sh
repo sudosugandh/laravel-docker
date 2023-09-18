0dc0c16c679890a85adb809aae9112710912db87
sugandh <ksugandh0@gmail.com>
2023-09-18 14:41:12 +0530

    rollback

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-# Function to run SSH commands securely with password authentication
-run_ssh_with_password() {
-    local host="$1"
-    local username="$2"
-    local password="$3"
-    local port="$4"
-    local script="$5"
-    sshpass -p "$password" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p "$port" "$username@$host" "$script"
-}
+# Navigate to your Laravel project directory
+cd /var/www/html/test-action
 
-# Enable Maintenance Mode on Server 1
-run_ssh_with_password "${{ secrets.SERVER1_HOST }}" "${{ secrets.SERVER1_USERNAME }}" "${{ secrets.SERVER1_PASSWORD }}" 22 "cd /var/www/html/test-action && php artisan down"
+# Activate a virtual environment, if applicable
 
-# Deploy Code on Server 2
-run_ssh_with_password "${{ secrets.SERVER2_HOST }}" "${{ secrets.SERVER2_USERNAME }}" "${{ secrets.SERVER2_PASSWORD }}" 22 "cd /var/www/html/test-action && sh deploy.sh"
+# Pull the latest code from your Git repository
+git pull origin main
 
-# Disable Maintenance Mode on Server 1
-run_ssh_with_password "${{ secrets.SERVER1_HOST }}" "${{ secrets.SERVER1_USERNAME }}" "${{ secrets.SERVER1_PASSWORD }}" 22 "cd /var/www/html/test-action && php artisan up"
+# Install or update Composer dependencies
+composer install --no-dev --no-interaction --prefer-dist
+
+# Run database migrations and other deployment tasks
+php artisan migrate --force
+# Any other necessary deployment tasks
+
+# Restart your web server, e.g., if you're using Nginx or Apache
+# systemctl restart nginx
+
+# If everything is successful, set the deployment success flag
+if [ $? -eq 0 ]; then
+  echo "Deployment successful"
+  exit 0
+else
+  echo "Deployment failed"
+  exit 1
+fi
